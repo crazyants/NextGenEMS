b814f56ebe72da896b6e9d8a6a5c94665a292338
Ben Lenington <ben2lening@gmail.com>
2018-04-16 19:12:00 -0500

    Create new OxygenLevel class and add new Enums

---
+++ NextGenEMS/Vitals/OxygenLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenEMS.Vitals
+{
+    public class OxygenLevel
+    {
+        public int OxygenPercent { get; set; }
+
+        public VitalsEnums.OxygenReadingMethod OxygenReadingMethod;
+    }
+}
