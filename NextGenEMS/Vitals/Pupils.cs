20d3e448f4d1e7d8d59c3e061182052df2ce5e49
Ben Lenington <ben2lening@gmail.com>
2018-07-09 13:44:52 -0500

    Create new vitals class for pupils info

---
+++ NextGenEMS/Vitals/Pupils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenEMS.Vitals
+{
+    public class Pupils
+    {
+        [DataMember]
+        public VitalsEnums.Pupils Type { get; set; }
+
+        [DataMember]
+        public string Notes { get; set; }
+
+    }
+}
