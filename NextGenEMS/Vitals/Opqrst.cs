a97bc43b8cd13eceb7d300632f8dec4925f5dcf5
Ben Lenington <ben2lening@gmail.com>
2018-07-12 14:39:47 -0500

    Create seperate class for OPQRST info

---
+++ NextGenEMS/Vitals/Opqrst.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenEMS.Vitals
+{
+    public class Opqrst
+    {
+        [DataMember]
+        public string Onset { get; set; }
+
+        [DataMember]
+        public string Provocation { get; set; }
+
+        [DataMember]
+        public string QualityOfPain { get; set; }
+
+        [DataMember]
+        public string RegionAndRadiation { get; set; }
+
+        [DataMember]
+        public string Severity { get; set; }
+
+        [DataMember]
+        public string TimeSinceOnset { get; set; }
+
+        [DataMember]
+        public string ClarifyingQuestions { get; set; }
+    }
+}
